e29f78fc5dac9a644b6b583f0b5329b29bcf71c2
mamadrezabahador <100021985+mamadrezabahador@users.noreply.github.com>
2022-05-08 13:55:08 -0700

    Create vt.py

---
+++ vt.py
@@ -0,0 +1,10 @@
+lst = [10, -4, 11, 29]
+(a) What expression represents the very first element of lst? print(lst[0])
+(b) What expression represents the very last element of lst? print(lst[3])
+(c) What is lst[0]? 10
+(d) What is lst[3]? 29
+(e) What is lst[1]? -4
+(f) What is lst[-1]? 29
+(g) What is lst[-4]? 10
+(h) Is the expression lst[3.0] legal or illegal? No becuse list indices must be 
+integers or slices, not float
